ca326baf262ec6cc77a1529f65f65562d43f23ef
Muchai248 <muchaimark6@gmail.com>
2023-09-19 22:59:20 +0300

    new commit

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+@app.route("/")
+def index():
+    return "My Flask App"
+
+if __name__== '__main__':
+ app.run(port=555)
